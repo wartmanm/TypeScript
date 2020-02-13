44f2ba8733a2bcb811d5a5e590bfcced8ab2b174
wartmanm <wartmanm@tutanota.com>
2020-02-13 11:37:33 -0600

    Update tests for JSX options

--- src/testRunner/unittests/config/commandLineParsing.ts
+++ src/testRunner/unittests/config/commandLineParsing.ts
@@ -111,7 +111,7 @@ namespace ts {
                         start: undefined,
                         length: undefined,
                     }, {
-                        messageText: "Argument for '--jsx' option must be: 'preserve', 'react-native', 'react'.",
+                        messageText: "Argument for '--jsx' option must be: 'preserve', 'react-native', 'react', 'vue'.",
                         category: Diagnostics.Argument_for_0_option_must_be_Colon_1.category,
                         code: Diagnostics.Argument_for_0_option_must_be_Colon_1.code,
 
--- tests/baselines/reference/api/tsserverlibrary.d.ts
+++ tests/baselines/reference/api/tsserverlibrary.d.ts
@@ -2751,7 +2751,8 @@ declare namespace ts {
         None = 0,
         Preserve = 1,
         React = 2,
-        ReactNative = 3
+        ReactNative = 3,
+        Vue = 4
     }
     export enum ImportsNotUsedAsValues {
         Remove = 0,
